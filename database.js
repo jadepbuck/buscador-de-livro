a82a04ac2cfc0fd45e5a085f6ddee4643a91741f
Jade Buck <jadepbuck@gmail.com>
2021-07-13 21:48:16 -0300

    First commit

---
+++ database.js
@@ -0,0 +1,42 @@
+const livros = [
+    {
+        id: 1,
+        nome:"Digital Minimalis",
+        autor: "Cal Newport",
+        categoria: "Produtividade",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    } ,
+    {
+        id: 2,
+        nome:"O genocídio negro brasileiro",
+        autor: "Abdias do Nascimento",
+        categoria: "Brasileiro",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    } ,
+    {
+        id: 3,
+        nome:"As veias abertas da América Latina",
+        autor: "Eduardo Galeano",
+        categoria: "Brasileiro",
+        paginas: 400,
+        recomenda: false,
+        leu: false
+    } ,
+    {
+        id: 4,
+        nome:"Algoritmos para viver",
+        autor: "Bian Christian",
+        categoria: "Tecnologia",
+        paginas: 412,
+        recomenda: true,
+        leu: true
+    } 
+    
+
+]
+
+module.exports = livros 
